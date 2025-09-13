12c02785bdef65cceb21656c6e64be45d48a25a5
Ahiram <146358742+Ahiram15@users.noreply.github.com>
2025-09-13 20:18:00 +0530

    updated from Sqlite to PostgreSQL

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 CORS(app)  # Allow cross-origin requests from frontend
 
 # Configure the database file
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///rtrwh_data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
@@ -587,9 +587,5 @@ if __name__ == '__main__':
     with app.app_context():
         # Create the database tables if they don't exist
         db.create_all()
-
-import os
-
-if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
